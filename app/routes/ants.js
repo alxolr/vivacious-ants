b9d92a64edb9d70d3187801d0feebb5ba1cc8fcc
Alexandru Olaru <alxolr@gmail.com>
2018-03-25 22:02:43 +0300

    Wired the front with the backend

--- app/routes/ants.js
+++ app/routes/ants.js
@@ -1,12 +1,34 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const net = require('net');
+const nos = require('net-object-stream');
+const router = express.Router();
+
+const { ANTSCOUNTER_SERVICE_HOST, ANTSCOUNTER_SERVICE_PORT } = process.env;
 
 router.get('/', function (req, res, next) {
-  res.render('ants', { title: 'Ants page' });
+  res.render('ants', { title: 'Ants page', count: 0, ants: '' });
 });
 
 router.post('/count', (req, res, next) => {
-  
+  const { ants } = req.body;
+  const client = createClient('count', {
+    host: ANTSCOUNTER_SERVICE_HOST,
+    port: ANTSCOUNTER_SERVICE_PORT,
+  });
+
+  const role = 'antscounter';
+  const cmd = 'count';
+
+  client.once('data', (data) => {
+    const { result } = data;
+    res.render('ants', { title: 'Ants page', count: result, ants });
+  });
+
+  client.write({ role, cmd, ants });
 });
+
+function createClient(ns, opts) {
+  return createClient[ns] || (createClient[ns] = nos(net.connect(opts)));
+}
 
 module.exports = router;
